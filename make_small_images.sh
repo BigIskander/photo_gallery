6ec26f9d644bc56cfe7b6d6b04b8809b663f53f8
BigIskander <bigiskander@gmail.com>
2024-04-14 20:21:27 +0300

    Add parameters.

--- make_small_images.sh
+++ make_small_images.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-cd ./images
-for f in *.jpg; do
-    echo ../output/$f
-    ffmpeg -i $f -vf scale=w=150:h=150:force_original_aspect_ratio=decrease ../output/$f
+images_folder="./images"
+file_extensions='(.jpg|.jpeg)$'
+miniatures_folder="./output"
+square_size=150
+# make miniatures
+for f in $(ls $images_folder/* | grep -E -i $file_extensions); do
+    file=$(basename $f)
+    ffmpeg -i $f -vf scale=w=$square_size:h=$square_size:force_original_aspect_ratio=decrease $miniatures_folder/$file
 done
-cd ..
