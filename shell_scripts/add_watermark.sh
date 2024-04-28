854adf5704da79bd8c19b3c2023f7b06479fe87d
BigIskander <bigiskander@gmail.com>
2024-04-28 21:34:53 +0300

    Watermark.

--- shell_scripts/add_watermark.sh
+++ shell_scripts/add_watermark.sh
@@ -3,7 +3,7 @@ watermark_image="../watermark_small.png"
 images_folder="../images"
 file_extensions='(.jpg|.jpeg)$'
 miniatures_folder="../output3"
-# make miniatures
+# add watermark
 for f in $(ls $images_folder/* | grep -E -i $file_extensions); do
     file=$(basename $f)
     ffmpeg -i $f -i $watermark_image -filter_complex "overlay=W-w-5:H-h-5" $miniatures_folder/$file
