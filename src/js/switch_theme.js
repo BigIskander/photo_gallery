05c52aac818c8fa09dcba0ab984b069591a3a75b
BigIskander <bigiskander@gmail.com>
2024-09-22 22:45:09 +0300

    Small fix.

--- src/js/switch_theme.js
+++ src/js/switch_theme.js
@@ -1,7 +1,8 @@
+var theme = undefined;
+
 //Switch do dark | light theme before page load if set up
 if(sessionStorage) {
-    var theme = undefined;
-    if(sessionStorage) theme = sessionStorage.getItem("theme")
+    theme = sessionStorage.getItem("theme")
     if(!theme) {
         if(window.matchMedia) {
             if(window.matchMedia('(prefers-color-scheme: dark)').matches) theme = 'dark'
@@ -9,16 +10,15 @@ if(sessionStorage) {
     }
     if(!theme || theme == "light") {
         document.body.className = ''
-        if(sessionStorage) sessionStorage.setItem("theme", "light")
+        sessionStorage.setItem("theme", "light")
     } else {
         document.body.className = 'dark'
-        if(sessionStorage) sessionStorage.setItem("theme", "dark")
+        sessionStorage.setItem("theme", "dark")
     }
 }
 
 //Swich theme by pressing button or link
 function switch_theme() {
-    var theme = undefined;
     if(sessionStorage) theme = sessionStorage.getItem("theme")
     if(!theme || theme == "light") {
         document.body.className = 'dark'
